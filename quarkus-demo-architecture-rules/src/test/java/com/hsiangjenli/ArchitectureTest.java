3687f21c0b68bbbf4881e76e9db32ca4d29af853
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-18 14:50:41 +0800

    refactor: simplify ArchitectureTest by using Taikai directly

--- quarkus-demo-architecture-rules/src/test/java/com/hsiangjenli/ArchitectureTest.java
+++ quarkus-demo-architecture-rules/src/test/java/com/hsiangjenli/ArchitectureTest.java
@@ -1,13 +1,12 @@
 package com.hsiangjenli;
 
-import com.enofex.taikai.Taikai.Builder;
+import com.enofex.taikai.Taikai;
 import org.junit.jupiter.api.Test;
 
 public class ArchitectureTest {
 
   @Test
   void shouldValidateJavaRules() {
-    Builder builder = new Builder();
-    builder.namespace("com.hsiangjenli").java(ArchitectureRules.BASE_JAVA_RULES).build().checkAll();
+    Taikai.builder().namespace("com.hsiangjenli").java(ArchitectureRules.BASE_JAVA_RULES).build().checkAll();
   }
 }
